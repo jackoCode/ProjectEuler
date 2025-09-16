91f1edbc620e5038c33ede852b63437f70a26ebf
jackoCode <jackocode.dev@gmail.com>
2025-09-16 12:12:13 +0200

    updated

--- src/P50ConsecutivePrimeSum.java
+++ src/P50ConsecutivePrimeSum.java
@@ -1,4 +1,10 @@
 public class P50ConsecutivePrimeSum {
+    // The prime 41, can be written as the sum of six consecutive primes:
+    // 41 = 2 + 3 + 5 + 7 + 13
+    // This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+    // The longest sum of consecutive primes below one-thousand that adds to a prime,
+    // contains 21 terms, and is equal to 953.
+    // Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
     // Solution --> 997651
 
@@ -7,7 +13,7 @@ public class P50ConsecutivePrimeSum {
         long summe = 0;
         long counter = 0;
 
-        while (summe < 1000000){
+        while (true){
 
             if(isPrime(counter)){
                 summe += counter;
@@ -28,11 +34,8 @@ public class P50ConsecutivePrimeSum {
     }
 
     public static boolean isPrime(long n){
-        boolean istPrim = true;
+        boolean istPrim = n >= 2;
 
-        if(n < 2){
-            istPrim = false;
-        }
         if(n % 2 == 0){
             istPrim = false;
         }
@@ -40,9 +43,10 @@ public class P50ConsecutivePrimeSum {
             istPrim = true;
         }
 
-        for (int i = 3; i * i <= n; i+=2) {
-            if(n % i == 0){
+        for (int i = 3; (long) i * i <= n; i+=2) {
+            if (n % i == 0) {
                 istPrim = false;
+                break;
             }
         }
 
